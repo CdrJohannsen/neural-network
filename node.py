68106b0b99839bf3b0ea7d18b3604f56e43caa48
CdrJohannsen <eisbaer.j.m@gmail.com>
2023-01-30 22:55:11 +0100

    update

--- node.py
+++ node.py
@@ -6,11 +6,14 @@ class Node():
         self.bias=bias
         self.links=links
         self.layers=layers
+        if self.index[0] != 0:
+            self.prevL=layers[self.index[0]-1]
+        if self.index[0] != 3:
+            self.nextL=layers[self.index[0]+1]
     
-    def calcSigmoid(self,x)
+    def calcSigmoid(self,x):
         return 1/(1+pow(math.e,-x)
         
-    def 
-    
     def test(self,value):
-        pass
+        for i in range(len(self.nextL)):
+            self.nextL[i].test(value*self.links[self.index[0]][self.index[1]][i])
