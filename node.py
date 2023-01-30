96e51d6dc8347301f1156d8addc4d859b9ef8b6e
CdrJohannsen <eisbaer.j.m@gmail.com>
2023-01-30 23:17:52 +0100

    fixed test function

--- node.py
+++ node.py
@@ -6,6 +6,7 @@ class Node():
         self.bias=bias
         self.links=links
         self.layers=layers
+        self.values=[]
         if self.index[0] != 0:
             self.prevL=layers[self.index[0]-1]
         if self.index[0] != 3:
@@ -15,5 +16,15 @@ class Node():
         return 1/(1+pow(math.e,-x)
         
     def test(self,value):
+        self.values.append(value)
+
+    def propagate(self):
+        value = self.calcValue()
+        if self.index[0]==3:
+            print(self.index[1],': ',value)
         for i in range(len(self.nextL)):
             self.nextL[i].test(value*self.links[self.index[0]][self.index[1]][i])
+
+    def calcValue(self):
+        # calculate sigmoid out of self.values and the bias
+        pass
